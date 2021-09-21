054145722e26a02c87760142ecffc35ee7c4e3f9
Valentina Bacherer <valentina.bacherer@gmail.com>
2021-09-21 11:30:36 -0300

    refactor names

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,38 @@ import './App.css';
 
 interface BlogProps {
   children: ReactNode;
-  titulo: string;
+  title: string;
 }
 
-interface ArticuloProps {
+interface ArticleProps {
   children: ReactNode;
-  contenido: string;
+  content: string;
 }
 
 const Blog: React.FC<BlogProps> = (props) => (
   <div className='container'>
-    <h1 className='titulo'>{props.titulo}</h1>
+    <h1 className='title'>{props.title}</h1>
     {props.children}
   </div>
 );
 
-const Articulo: React.FC<ArticuloProps> = (props) => (
-  <div className='tarjeta'>
+const Article: React.FC<ArticleProps> = (props) => (
+  <div className='card'>
     <h1 className='container'>{props.children}</h1>
-    <p children={props.contenido} className='contenido' />
+    <p children={props.content} className='content' />
   </div>
 );
 
 function App() {
-  const contenidoDeclarativo =
+  const declarativeContent =
     'React te ayuda a crear interfaces de usuario interactivas de forma sencilla. Diseña vistas simples para cada estado en tu aplicación, y React se encargará de actualizar y renderizar de manera eficiente los componentes correctos cuando los datos cambien.';
-  const contenidoComponentes =
+  const componentsContent =
     'Con React creas componentes encapsulados que manejan su propio estado, y los conviertes en interfaces de usuario complejas. Ya que la lógica de los componentes está escrita en JavaScript y no en plantillas, puedes pasar datos de forma sencilla a través de tu aplicación y mantener el estado fuera del DOM.';
 
   return (
-    <Blog titulo='CARACTERÍSTICAS DE REACT'>
-      <Articulo contenido={contenidoDeclarativo}>Declarativo</Articulo>
-      <Articulo contenido={contenidoComponentes}>
-        Basado en Componentes
-      </Articulo>
+    <Blog title='CARACTERÍSTICAS DE REACT'>
+      <Article content={declarativeContent}>Declarativo</Article>
+      <Article content={componentsContent}>Basado en Componentes</Article>
     </Blog>
   );
 }
